32321a3c55ca0abb925730ff77d5e968d96d0518
AuroraVane <47216870+AuroraVane@users.noreply.github.com>
2024-07-21 14:37:07 +0800

    Version 1.0.3
    - Overhauled the code to store information via a separate class 2D array
    - Movement is broken but will be reimplemented

--- env.py
+++ env.py
@@ -1,105 +1,107 @@
 import pygame
-
-# Initialize Pygame
-pygame.init()
+import FEengine
 
 # Set up the display
-screen_width = 640
-screen_height = 440
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("RPG-FE")
+screen_width = 600
+screen_height = 400
+MAX_FPS = 15
 
 # Initialize size
-square_width = square_height = 50
-border_width = border_height = 20
+square_width = player_width = player_height = 50
 
-
-# Generate a 12*8 grid
-matrix = [['0'] * 12 for _ in range(8)]
-
-# Initialize player positions
-player1_x = border_width
-player1_y = border_height
-player2_x = screen_width - square_width - border_width
-player2_y = screen_height - square_height - border_height
-
-player1_coed = (0,0)
-player2_coed = (11,7)
-
-matrix[player1_coed[1]][player1_coed[0]] = 'A'
-matrix[player2_coed[1]][player2_coed[0]] = 'B'
-
-player_width = square_width
-player_height = square_height
-
-player1_color = (255, 0, 0)
-player2_color = (0, 0, 255)
-
-# Flags
-player1_select = False
-player2_select = False
+#Initialise image
+IMAGES = {}
+def loadImages():
+    units = ['pUnit','eUnit']
+    for unit in units:
+        IMAGES[unit] = pygame.transform.scale(pygame.image.load("images/"+unit+".png"),(square_width,square_width))
 
 # Game loop
-running = True
-while running:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def main():
 
-        # Player Select
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.Rect(player1_x, player1_y, player_width, player_height).collidepoint(event.pos):
-                player1_color = (255, 255, 0)  # Change player 1 color to yellow
-                player2_color = (0, 0, 255)
-                player1_select = True
-                player2_select = False
-            elif pygame.Rect(player2_x, player2_y, player_width, player_height).collidepoint(event.pos):
-                player2_color = (255, 255, 0)  # Change player 2 color to yellow
-                player1_color = (255, 0, 0)
-                player2_select = True
-                player1_select = False
-            else:
-                if player1_select:
-                    col = (event.pos[0] - border_width) // square_width
-                    row = (event.pos[1] - border_height) // square_height
-                    player1_x = border_width + col * square_width
-                    player1_y = border_height + row * square_height
-                    player1_select = False
-                    print(col,row)
-                    matrix[player1_coed[1]][player1_coed[0]] = '0'
-                    matrix[col][row] = 'A'
-                elif player2_select:
-                    col = (event.pos[0] - border_width) // square_width
-                    row = (event.pos[1] - border_height) // square_height
-                    player2_x = border_width + col * square_width
-                    player2_y = border_height + row * square_height
+    # Initialize Pygame
+    pygame.init()
+
+    #Display
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("RPG-FE")
+    ICON = pygame.image.load('images/wp.png')
+    pygame.display.set_icon(ICON)
+
+    #System Requisites
+    clock = pygame.time.Clock()
+    gs = FEengine.GameState() #gamestate
+    loadImages()
+
+    running = True
+    while running:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # Player Select
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.Rect(player1_x, player1_y, player_width, player_height).collidepoint(event.pos):
+                    player1_color = (255, 255, 0)  # Change player 1 color to yellow
+                    player2_color = (0, 0, 255)
+                    player1_select = True
                     player2_select = False
-                    matrix[player2_coed[1]][player2_coed[0]] = '0'
-                    matrix[col][row] = 'B'
+                elif pygame.Rect(player2_x, player2_y, player_width, player_height).collidepoint(event.pos):
+                    player2_color = (255, 255, 0)  # Change player 2 color to yellow
+                    player1_color = (255, 0, 0)
+                    player2_select = True
+                    player1_select = False
+                else:
+                    if player1_select:
+                        col = (event.pos[0]) // square_width
+                        row = (event.pos[1]) // square_width
+                        player1_x = col * square_width
+                        player1_y = row * square_width
+                        player1_select = False
+                        print(col,row)
+                    elif player2_select:
+                        col = (event.pos[0]) // square_width
+                        row = (event.pos[1]) // square_width
+                        player2_x = col * square_width
+                        player2_y = row * square_width
+                        player2_select = False
 
-                player1_color = (255, 0, 0)
-                player2_color = (0, 0, 255)
-                print(matrix)
+                    player1_color = (255, 0, 0)
+                    player2_color = (0, 0, 255)
 
+        drawGameState(screen,gs)
+
+        clock.tick(MAX_FPS)
+        pygame.display.flip()
+        pygame.display.update()
+
+    # Quit Pygame
+    pygame.quit()
+
+def drawGameState(screen,gs):
+    drawBoard(screen) #Draw squares on the board
+    drawUnits(screen,gs.board) #Draw Units on top of the squares
+
+def drawBoard(screen):
+    # Draw board
     # Render graphics
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(0, 0, screen_width, screen_height))
-    # Draw chessboard
     for row in range(8):
         for col in range(12):
             if (row + col) % 2 == 0:
-                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(border_width + (col * square_width), border_height + (row * square_height), square_width, square_height))
+                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect((col * square_width), (row * square_width), square_width, square_width))
             else:
-                pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(border_width + (col * square_width), border_height + (row * square_height), square_width, square_height))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, screen_width, border_height))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, border_width, screen_height))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, screen_height - border_height, screen_width, border_height))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(screen_width - border_width, 0, border_width, screen_height))
-    pygame.draw.rect(screen, player1_color, pygame.Rect(player1_x, player1_y, player_width, player_height))
-    pygame.draw.rect(screen, player2_color, pygame.Rect(player2_x, player2_y, player_width, player_height))
+                pygame.draw.rect(screen, (196, 164, 132), pygame.Rect((col * square_width), (row * square_width), square_width, square_width))
+        for col in range(2):
+            pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(((5+col) * square_width), (row * square_width), square_width, square_width))
+    
+def drawUnits(screen,board):
+    for row in range(8):
+        for col in range(12):
+            unit = board[row][col]
+            if unit != "--":
+                screen.blit(IMAGES[unit],pygame.Rect(col*square_width, row*square_width, square_width, square_width))
 
-    pygame.display.flip()
-
-# Quit Pygame
-pygame.quit()
+if __name__ == "__main__":
+    main()
