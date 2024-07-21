51dc295685d0d7df42fb0d22bd1213d719abb935
AuroraVane <47216870+AuroraVane@users.noreply.github.com>
2024-07-21 10:18:18 +0800

    Version 1.0.2
    Movement Patch

--- env.py
+++ env.py
@@ -12,13 +12,22 @@ pygame.display.set_caption("RPG-FE")
 # Initialize size
 square_width = square_height = 50
 border_width = border_height = 20
-running = True
+
+
+# Generate a 12*8 grid
+matrix = [['0'] * 12 for _ in range(8)]
 
 # Initialize player positions
 player1_x = border_width
 player1_y = border_height
 player2_x = screen_width - square_width - border_width
 player2_y = screen_height - square_height - border_height
+
+player1_coed = (0,0)
+player2_coed = (11,7)
+
+matrix[player1_coed[1]][player1_coed[0]] = 'A'
+matrix[player2_coed[1]][player2_coed[0]] = 'B'
 
 player_width = square_width
 player_height = square_height
@@ -31,6 +40,7 @@ player1_select = False
 player2_select = False
 
 # Game loop
+running = True
 while running:
     # Handle events
     for event in pygame.event.get():
@@ -56,15 +66,21 @@ while running:
                     player1_x = border_width + col * square_width
                     player1_y = border_height + row * square_height
                     player1_select = False
+                    print(col,row)
+                    matrix[player1_coed[1]][player1_coed[0]] = '0'
+                    matrix[col][row] = 'A'
                 elif player2_select:
                     col = (event.pos[0] - border_width) // square_width
                     row = (event.pos[1] - border_height) // square_height
                     player2_x = border_width + col * square_width
                     player2_y = border_height + row * square_height
                     player2_select = False
+                    matrix[player2_coed[1]][player2_coed[0]] = '0'
+                    matrix[col][row] = 'B'
 
                 player1_color = (255, 0, 0)
                 player2_color = (0, 0, 255)
+                print(matrix)
 
     # Render graphics
     screen.fill((0, 0, 0))
